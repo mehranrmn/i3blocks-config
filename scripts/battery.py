7a4b0b62c641bd53ddf0aa96c498f8b1a0218eec
mehran armiyon <mehranrmn@gmail.com>
2020-04-12 12:49:27 +0430

    Update battery.py

--- scripts/battery.py
+++ scripts/battery.py
@@ -4,25 +4,25 @@
 
 import fontawesome as fa
 
-CapacityPath = '/sys/class/power_supply/BAT0/capacity'
-StatusPath = '/sys/class/power_supply/BAT0/status'
-with open(CapacityPath,'r') as CAP:
-     capacity = CAP.read().replace('\n','')
+CapacityPath = "/sys/class/power_supply/BAT0/capacity"
+StatusPath = "/sys/class/power_supply/BAT0/status"
+with open(CapacityPath,"r") as CAP:
+     capacity = CAP.read().replace("\n","")
      pass
-with open(StatusPath,'r') as STA:
-     status = STA.read().replace('\n','')
+with open(StatusPath,"r") as STA:
+     status = STA.read().replace("\n","")
      pass
 
 if status == "Discharging" :
     if int(capacity) > 90:
-        print(fa.icons['battery-full']+' '+capacity+'%')
+        print(fa.icons["battery-full"]+" "+capacity+"%")
     elif 75 < int(capacity) <= 90:
-        print(fa.icons['battery-three-quarters']+' '+capacity+'%')
+        print(fa.icons["battery-three-quarters"]+" "+capacity+"%")
     elif 50 < int(capacity) <= 75:
-        print(fa.icons['battery-half']+' '+capacity+'%')
+        print(fa.icons["battery-half"]+" "+capacity+"%")
     elif 20 < int(capacity) <= 50:
-        print(fa.icons['battery-quarter']+' '+capacity+'%')
+        print(fa.icons["battery-quarter"]+" "+capacity+"%")
     elif int(capacity) <= 20:
-         print(fa.icons['battery-empty']+' '+capacity+'%')
+         print(fa.icons["battery-empty"]+" "+capacity+"%")
 else:
-    print(fa.icons['plug']+' '+capacity+'%')
+    print(fa.icons["plug"]+" "+capacity+"%")
